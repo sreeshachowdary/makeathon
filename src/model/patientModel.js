a42d7b4a1a92ee8809509d554c3800d56b5c5a01
sreeshachowdary <sreeshachowdary51@gmail.com>
2025-01-27 17:59:00 +0530

    Changing the Model

--- src/model/patientModel.js
+++ src/model/patientModel.js
@@ -1,33 +1,29 @@
 const mongoose = require('mongoose');
 
+
 const patientSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phoneNumber: { type: Number },
-    address: { type: String },
-    bloodGroup: { type: String },
-    birthDate: { type: Date },  
-    role: {type: String, required: true},
-    adharCardNumber: {type: String, required: true, unique: true},
-    height: {type: Number, required: true},
-    weight: {type: Number, required: true},
-    bloodPressure: {type: Number, required: true},
-    heartRate: {type: Number, required: true},
-    bmi: {type: Number, required: true},
-    primaryPhysicianName: {type: String, required: true},
-    primaryPhysicianContact: {type: String, required: true},
-    allergies: {type: Array, required: true},
-    currentMedicines: {type: Array, required: true},
-    surgicalHistory: {type: String, required: true},
-    familyMedicalHistory: {type: String, required: true},
-    mentalHealthIssue: {type: String, required: true},
-    physicalDissability: {type: String, required: true},
-    dailySteps: {type: Number, required: true},
-    dailyWaterIntake: {type: Number, required: true},
-    sleepHours: {type: Number, required: true},
-    languagePreference: {type: String, required: true}
-});
+    id: {
+        type: String,
+        required: true
+    },
+    name: {
+      type: String
+    },
+    age: {
+      type: Number
+    },
+    wellnessGoals: {
+      type: [String]
+    },
+    preventiveCareReminders: {
+      type: [String],
+      required: false, 
+    },
+    healthTips: {
+        type: [String],
+        required: false,
+    }
+  });
 
 const Patient = mongoose.model('Patient', patientSchema);
 module.exports = Patient;
-// name, email, pwd, phoneNumber, address,bloodGroup,birthDate,adharCardNumber, height, weight, BloodPressure, HeartRate, BMI, role, primaryPhysicianName, primaryPhysicianContact, allergies, currentMedicines, SurgicalHistory, familyMedicalHistory, MentalHealthIssue, PhysicalDissability, languagePreference, dailySteps, dailyWaterIntake, sleepHours
--- src/routes/patientRoutes.js
+++ src/routes/patientRoutes.js
@@ -26,4 +26,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// fetch patient info
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const patient = await Patient.findById(id);
+    if (!patient) return res.status(404).json({ message: 'Patient not found' });
+
+    res.json({ patient });
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching patient info', error: err });
+  }
+});
+
 module.exports = router;
